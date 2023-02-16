619ecb35e6f47e0d1f868980ca6d2e4183cd9310
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-02-16 11:52:25 +0530

    1 file modified

--- src/Justin.js
+++ src/Justin.js
@@ -10,7 +10,7 @@ export default function Justin(obj) {
     <div>
       <h1>Hello Justin</h1>
       <p>My name is {obj.name}</p>
-      <button onClick={Showmyage}>click</button>
+      <label htmlFor=""> Click to see Age </label><button onClick={Showmyage}>click</button>
       {2===2? <h1>True</h1>:<h1>False</h1>}
       <label htmlFor=""> Click to Recent issue </label> <button onClick={obj.clickfn}>click</button>
       {[1,2,3,4,5,6,7,8,9,10].map((num) => (<h1>{num*2}</h1>))}
